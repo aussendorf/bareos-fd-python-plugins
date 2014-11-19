39da4c386a36271002c88eff9a0762aed4f8b2a7
aussendorf <maik.aussendorf@bareos.org>
2014-11-19 16:22:38 +0100

    sample from osbconf

---
+++ plugin/BareosFdPluginFileInteract.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Bareos python class as stub for option plugin
+# handle_backup_file gets called for each file in the backup
+# 
+# (c) Bareos GmbH & Co. KG
+# AGPL v.3
+# Author: Maik Aussendorf
+
+from bareosfd import *
+from bareos_fd_consts import *
+import os
+from  BareosFdPluginBaseclass import *
+import BareosFdWrapper
+
+class BareosFdPluginFileInteract  (BareosFdPluginBaseclass):
+
+    def handle_backup_file(self,context, savepkt):
+        DebugMessage(context, 100, "handle_backup_file called with " + str(savepkt) + "\n");
+        DebugMessage(context, 100, "fname: " + savepkt.fname + " Type: " + str(savepkt.type) + "\n");
+        if ( savepkt.type == bFileType['FT_REG'] ):
+            #DebugMessage(context, 100, "regulaer file, do something now...\n");
+            #JobMessage(context, bJobMessageType['M_INFO'], "Option Plugin file interact on" + savepkt.fname + "\n");
+            #JobMessage(context, bJobMessageType['M_ABORT'], "Option Plugin file interact on" + savepkt.fname + "\n");
+            JobMessage(context, bJobMessageType['M_WARNING'], "Option Plugin file interact on" + savepkt.fname + "\n");
+            # add your stuff here
+
+        #return bRCs['bRC_OK'];
+        return bRCs['bRC_Stop'];
+        #return bRCs['bRC_Term'];
+        #return bRCs['bRC_Error'];
+
+    
